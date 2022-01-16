ede97057cd6a46f1f78a838952b2af4bbc8277f0
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-01-16 12:55:13 -0500

    added controller file

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+TO_DO_LIST = []
+print(TO_DO_LIST)
+
+@app.route('/')
+def index():
+    return render_template('index.html', to_do_list = TO_DO_LIST)
+
+@app.route('/todo', methods = ['POST'])
+def todo():
+    if not request.form.get('todo'):
+        render_template('index.html', to_do_list = TO_DO_LIST)
+    TO_DO_LIST.append(request.form.get('todo'))
+    print(TO_DO_LIST)
+    return render_template('index.html', to_do_list = TO_DO_LIST)
+
+@app.route('/clear', methods= ['POST'])
+def clear():
+    TO_DO_LIST.clear()
+    return render_template('index.html', to_do_list = TO_DO_LIST)
