e00aa23d058b6687667dd648caa6e017779b9620
Flavia Y. Ouyang <flavia.ouyang@mail.mcgill.ca>
2022-01-19 10:46:02 -0500

    fixed minor bug: add nothing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from sqlalchemy import null
 
 app = Flask(__name__)
 
@@ -6,16 +7,21 @@ TO_DO_LIST = []
 
 @app.route('/')
 def index():
+    print("/", TO_DO_LIST)
     return render_template('index.html', to_do_list = TO_DO_LIST)
 
 @app.route('/todo', methods = ['POST'])
 def todo():
-    if not request.form.get('todo'):
-        render_template('index.html', to_do_list = TO_DO_LIST)
-    TO_DO_LIST.append(request.form.get('todo'))
-    return render_template('index.html', to_do_list = TO_DO_LIST)
+    if request.form.get('todo') == '':
+        print("add nothing: ",TO_DO_LIST)
+        return render_template('index.html', to_do_list = TO_DO_LIST)
+    if request.form.get('todo') != '':
+        TO_DO_LIST.append(request.form.get('todo'))
+        print("append: ",TO_DO_LIST)
+        return render_template('index.html', to_do_list = TO_DO_LIST)
 
 @app.route('/clear', methods= ['POST'])
 def clear():
     TO_DO_LIST.clear()
+    print("clear: ",TO_DO_LIST)
     return render_template('index.html', to_do_list = TO_DO_LIST)
